f2aa36e0a3b509519e9e4927da31cf3b368cdbce
Rogan Scoundrel <1205203+roganscoundrel@users.noreply.github.com>
2022-04-02 20:56:13 -0600

    Initial app commit

---
+++ Invoke-PriorityReminder.ps1
@@ -0,0 +1,29 @@
+﻿Add-Type -AssemblyName Microsoft.VisualBasic
+
+$onTrackTimeoutInMinutes = 60
+$offTrackTimeoutInMinutes = 2
+
+$title = 'Priority Reminder'
+$onTrackAnswer = "Yes"
+
+$priority = [Microsoft.VisualBasic.Interaction]::InputBox('Please enter your current priority:', $title)
+if($priority.Length -le 0) {
+    Exit
+}
+
+function checkIfOnTrack {
+    if($onTrackAnswer -eq "Yes"){
+        Start-Sleep -Seconds ($onTrackTimeoutInMinutes * 60)
+    } 
+    if($onTrackAnswer -eq "No"){
+        Start-Sleep -Seconds ($offTrackTimeoutInMinutes * 60)
+    }
+    if($onTrackAnswer -eq "Cancel"){
+        Exit
+    }
+    
+    $onTrackAnswer = [System.Windows.MessageBox]::Show("Are you working on $($priority)?", $title,'YesNoCancel','Question')
+    checkIfOnTrack
+}
+
+checkIfOnTrack
